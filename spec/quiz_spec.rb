9e5f804c8558caa12ccbba382bdafc35f6035275
Alain Bloch <alainbloch@gmail.com>
2015-08-10 18:53:03 -0700

    Added tests for length.

--- spec/quiz_spec.rb
+++ spec/quiz_spec.rb
@@ -3,13 +3,26 @@ require 'spec_helper'
 describe Quiz do
  
 	before :each do
-	    @quiz = Quiz.new(3)
+    @length = 3
+	  @quiz = Quiz.new(@length)
 	end
 
 	describe "#new" do
-	    it "takes a single parameter and returns a Quiz object" do
-	        @quiz.should be_an_instance_of Quiz
-	    end
+	 it "takes a single parameter and returns a Quiz object" do
+	   @quiz.should be_an_instance_of Quiz
+	 end
 	end
+
+  describe "#length" do
+    it "is equal to the argument passed in" do
+      @quiz.length.should equal(@length)
+    end
+
+    it "can be overwritten" do
+      new_length = 5
+      @quiz.length = new_length
+      @quiz.length.should equal(new_length)
+    end
+  end
 
 end
